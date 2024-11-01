5b89c43a736772d25a3f0a1ad7edd01844f00e90
Kirilliys123456 <kbarsukevic@gmail.com>
2024-11-01 12:12:25 +0300

    Commit

--- counts.py
+++ counts.py
@@ -13,7 +13,6 @@ def is_contains( string, list_to_search):
             return True
     return False
 
-
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
