f6c9b9855a144b59f37712b02c906fe29794c83c
Kirilliys123456 <kbarsukevic@gmail.com>
2024-11-15 17:17:06 +0300

    Commit

---
+++ modyle 4.1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as fa_di
+from true_math import divide as tr_di
+
+result1 = fa_di(69, 3)
+result2 = fa_di(3, 0)
+result3 = tr_di(49, 7)
+result4 = tr_di(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
